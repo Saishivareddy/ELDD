e672640c140c212f9d11bb33d25a9e2b55581c60
Saishivareddy <saishivareddy75@gmail.com>
2022-06-28 18:24:12 +0530

    File Operations

--- Native_Compilation/6_File_Operations/driver.c
+++ Native_Compilation/6_File_Operations/driver.c
@@ -11,7 +11,6 @@ MODULE_AUTHOR("Saishiva Reddy");
 MODULE_DESCRIPTION("Module to create a Character Device Driver");
 
 dev_t dev = 0;
-static struct class *dev_class;
 static struct cdev my_cdev;
 
 static int __init char_driver_init(void);
@@ -19,8 +18,8 @@ static void __exit char_driver_exit(void);
 
 static int my_open(struct inode *inode, struct file *file);
 static int my_release(struct inode *inode, struct file *file);
-static ssize_t my_read(struct file *filp, char __user *buf, size_t len, loff_t *off);
-static ssize_t my_write(struct file *filp, const char __user *buf, size_t len, loff_t *off);
+static ssize_t my_read(struct file *filp, char *buf, size_t len, loff_t *off);
+static ssize_t my_write(struct file *filp, const char *buf, size_t len, loff_t *off);
 
 static struct file_operations fops = 
 {
@@ -33,7 +32,7 @@ static struct file_operations fops =
 
 static int my_open(struct inode *inode, struct file *file)
 {
-	printk("\n Driver opened \n");
+	printk("\n Driver Opened \n");
 	return 0;
 }
 
@@ -43,16 +42,16 @@ static int my_release(struct inode *inode, struct file *file)
 	return 0;
 }
 
-static ssize_t my_read(struct file *filp, char __user *buf, size_t len, loff_t *off)
+static ssize_t my_read(struct file *filp, char *buf, size_t len, loff_t *off)
 {
 	printk("\n Driver Read \n");
-	return 0;
+	return len;
 }
 
-static ssize_t my_write(struct file *filp, const char __user *buf, size_t len, loff_t *off)
+static ssize_t my_write(struct file *filp, const char *buf, size_t len, loff_t *off)
 {
 	printk("\n Driver Write \n");
-	return 0;
+	return len;
 }
 
 static int __init char_driver_init(void)
@@ -70,31 +69,16 @@ static int __init char_driver_init(void)
 		printk("\n Cannot add device \n");
 		goto r_class;
 	}
-	if((dev_class = class_create(THIS_MODULE, "my_class"))== NULL)
-	{
-		printk("\n Cannot create device class\n");
-		goto r_class;
-	}
-	if(device_create(dev_class, NULL, dev, NULL, "My_device") == NULL)
-	{
-		printk("\n Cannot create Device \n");
-		goto r_device;
-	}
 	printk("\n Driver Inserted\n");
 	return 0;
 
 r_class :
 	unregister_chrdev_region(dev, 1);
 	return -1;
-r_device :
-	class_destroy(dev_class);
-	return -1;	
 }
 
 static void __exit char_driver_exit(void)
 {
-	device_destroy(dev_class, dev);
-	class_destroy(dev_class);
 	cdev_del(&my_cdev);
 	unregister_chrdev_region(dev, 1);
 	printk("\n Driver removed \n");
