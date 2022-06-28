cc36dcd8b609f54bebc74d31afcfa7c89b20390c
Saishivareddy <saishivareddy75@gmail.com>
2022-06-28 18:40:40 +0530

    File Operations on Simple Char Driver

--- Native_Compilation/6_File_Operations/driver.c
+++ Native_Compilation/6_File_Operations/driver.c
@@ -8,7 +8,7 @@
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Saishiva Reddy");
-MODULE_DESCRIPTION("Module to create a Character Device Driver");
+MODULE_DESCRIPTION("Module to create a Simple Device Driver(File Operations)");
 
 dev_t dev = 0;
 static struct cdev my_cdev;
@@ -18,39 +18,39 @@ static void __exit char_driver_exit(void);
 
 static int my_open(struct inode *inode, struct file *file);
 static int my_release(struct inode *inode, struct file *file);
-static ssize_t my_read(struct file *filp, char *buf, size_t len, loff_t *off);
+static ssize_t my_read(struct file *filp, char __user *buf, size_t len, loff_t *off);
 static ssize_t my_write(struct file *filp, const char *buf, size_t len, loff_t *off);
 
 static struct file_operations fops = 
 {
-	.owner = THIS_MODULE,
-	.read = my_read,
-	.write = my_write,
-	.open = my_open,
+	.owner	 = THIS_MODULE,
+	.read	 = my_read,
+	.write	 = my_write,
+	.open	 = my_open,
 	.release = my_release,
 };
 
 static int my_open(struct inode *inode, struct file *file)
 {
-	printk("\n Driver Opened \n");
+	pr_info("\n Driver Opened \n");
 	return 0;
 }
 
 static int my_release(struct inode *inode, struct file *file)
 {
-	printk("\n Driver Closed \n");
+	pr_info("\n Driver Closed \n");
 	return 0;
 }
 
-static ssize_t my_read(struct file *filp, char *buf, size_t len, loff_t *off)
+static ssize_t my_read(struct file *filp, char __user *buf, size_t len, loff_t *off)
 {
-	printk("\n Driver Read \n");
-	return len;
+	pr_info("\n Driver Read \n");
+	return 0;
 }
 
-static ssize_t my_write(struct file *filp, const char *buf, size_t len, loff_t *off)
+static ssize_t my_write(struct file *filp, const char __user *buf, size_t len, loff_t *off)
 {
-	printk("\n Driver Write \n");
+	pr_info("\n Driver Write \n");
 	return len;
 }
 
@@ -58,23 +58,18 @@ static int __init char_driver_init(void)
 {
 	if(alloc_chrdev_region(&dev, 0, 1, "Mydev") < 0)
 	{
-		printk("\n Error in allocating Major Number \n");
+		pr_err("\n Error in allocating Major Number \n");
 		return -1;
 	}
-	printk("\n Major = %d and Minor = %d\n", MAJOR(dev), MINOR(dev));
+	pr_info("\n Major = %d and Minor = %d\n", MAJOR(dev), MINOR(dev));
 
 	cdev_init(&my_cdev, &fops);	
 	if(cdev_add(&my_cdev, dev, 1) < 0)
 	{
-		printk("\n Cannot add device \n");
-		goto r_class;
+		pr_err("\n Cannot add device \n");
 	}
 	printk("\n Driver Inserted\n");
 	return 0;
-
-r_class :
-	unregister_chrdev_region(dev, 1);
-	return -1;
 }
 
 static void __exit char_driver_exit(void)
